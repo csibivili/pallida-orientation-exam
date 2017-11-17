8b763ab4344804aca10b184f3feb0a20446f3fc8
csibvili <csibi.vili@gmail.com>
2017-11-17 09:50:17 +0100

    add dbcontext to local mssql server

--- LicencePlateApp/LicencePlateApp/Startup.cs
+++ LicencePlateApp/LicencePlateApp/Startup.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using LicencePlateApp.Entities;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace LicencePlateApp
 {
@@ -10,6 +12,7 @@ namespace LicencePlateApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<LicencePlateContext>(options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LicencePlateDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
