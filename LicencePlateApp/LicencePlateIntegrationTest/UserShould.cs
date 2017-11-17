1d828dd3d95b480f7a5394d9581d5aa3ded54fb5
csibvili <csibi.vili@gmail.com>
2017-11-17 11:04:27 +0100

    diplomatic cars are searchable

--- LicencePlateApp/LicencePlateIntegrationTest/UserShould.cs
+++ LicencePlateApp/LicencePlateIntegrationTest/UserShould.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
 using LicencePlateApp.Repository;
+using LicencePlateApp.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LicencePlateIntegrationTest
 {
@@ -18,7 +21,6 @@ namespace LicencePlateIntegrationTest
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LicencePlateDb;Integrated Security=True;Connect Timeout=30;");
         }
 
-
         [Fact]
         public void SearchForCarByPlate()
         {
@@ -29,6 +31,34 @@ namespace LicencePlateIntegrationTest
                 var car = licencePlateService.SearchForCarByPlate("CICA-93");
 
                 Assert.Equal("CICA-93", car.Plate);
+            }
+        }
+
+        [Fact]
+        public void SearchForPoliceCars()
+        {
+            using (var licencePlateContext = new LicencePlateContext(optionsBuilder.Options))
+            {
+                var licencePlateService = new LicencePlateService(new LicencePlateRepository(licencePlateContext));
+
+                List<LicencePlate> policeCars = licencePlateService.PoliceCars();
+
+                Assert.Equal(policeCars.First().Plate.Substring(0,2), "RB");
+                Assert.Equal(policeCars.Last().Plate.Substring(0, 2), "RB");
+            }
+        }
+
+        [Fact]
+        public void SearchForDiplomatCars()
+        {
+            using (var licencePlateContext = new LicencePlateContext(optionsBuilder.Options))
+            {
+                var licencePlateService = new LicencePlateService(new LicencePlateRepository(licencePlateContext));
+
+                List<LicencePlate> policeCars = licencePlateService.DiplomatCars();
+
+                Assert.Equal(policeCars.First().Plate.Substring(0, 2), "DT");
+                Assert.Equal(policeCars.Last().Plate.Substring(0, 2), "DT");
             }
         }
     }
