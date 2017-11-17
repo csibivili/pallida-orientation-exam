15114ad40cdd797b180bb6b2ce4880e8f07165f0
csibvili <csibi.vili@gmail.com>
2017-11-17 10:02:59 +0100

    add repository layer

---
+++ LicencePlateApp/LicencePlateApp/Repository/LicencePlateRepository.cs
@@ -0,0 +1,14 @@
+﻿using LicencePlateApp.Entities;
+
+namespace LicencePlateApp.Repository
+{
+    public class LicencePlateRepository
+    {
+        private LicencePlateContext LicencePlateContext;
+
+        public LicencePlateRepository(LicencePlateContext licencePlateContext)
+        {
+            LicencePlateContext = licencePlateContext;
+        }
+    }
+}
