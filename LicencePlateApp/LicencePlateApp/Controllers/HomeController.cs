86646aae99de9431d5a4c58110bd0eb69de39101
csibvili <csibi.vili@gmail.com>
2017-11-17 09:41:29 +0100

    empty endpoints created based on the description

---
+++ LicencePlateApp/LicencePlateApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LicencePlateApp.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("/search")]
+        public IActionResult Search(string q, int police, int diplomat)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/search/{brand}")]
+        public IActionResult BrandSearch(string brand)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/api/search/{brand}")]
+        public IActionResult ApiBrandSearch(string brand)
+        {
+            return Json();
+        }
+    }
+}
