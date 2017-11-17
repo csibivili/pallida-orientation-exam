9ed94e9fe701fa696d712df33c8d071d8723fa64
csibvili <csibi.vili@gmail.com>
2017-11-17 10:12:41 +0100

    service layer & repository injected

--- LicencePlateApp/LicencePlateApp/Startup.cs
+++ LicencePlateApp/LicencePlateApp/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using LicencePlateApp.Repository;
+using LicencePlateApp.Service;
 
 namespace LicencePlateApp
 {
@@ -14,6 +16,8 @@ namespace LicencePlateApp
             services.AddMvc();
             services.AddEntityFrameworkSqlServer()
                 .AddDbContext<LicencePlateContext>(options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LicencePlateDb;Integrated Security=True;Connect Timeout=30;"));
+            services.AddScoped<LicencePlateRepository>();
+            services.AddScoped<LicencePlateService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
