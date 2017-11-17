659700bdc4980a71a629a9209c64a69b5f657492
csibvili <csibi.vili@gmail.com>
2017-11-17 12:46:44 +0100

    model for json result

---
+++ LicencePlateApp/LicencePlateApp/ViewModels/CarList.cs
@@ -0,0 +1,11 @@
+﻿using LicencePlateApp.Models;
+using System.Collections.Generic;
+
+namespace LicencePlateApp.ViewModels
+{
+    public class CarList
+    {
+        public string Result { get; set; }
+        public List<LicencePlate> Data { get; set; }
+    }
+}
