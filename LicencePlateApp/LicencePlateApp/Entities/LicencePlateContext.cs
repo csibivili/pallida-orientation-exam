c6590ff346a402d6fca7a77482197a4d76ff2f81
csibvili <csibi.vili@gmail.com>
2017-11-17 09:46:44 +0100

    licencePlateContext has been added

---
+++ LicencePlateApp/LicencePlateApp/Entities/LicencePlateContext.cs
@@ -0,0 +1,15 @@
+﻿using LicencePlateApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicencePlateApp.Entities
+{
+    public class LicencePlateContext : DbContext
+    {
+        public LicencePlateContext(DbContextOptions<LicencePlateContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<LicencePlate> Licence_plates { get; set; }
+    }
+}
